f1cb2ea0d1cdff1f64ff76e6125a83c4f615f489
ambrosia13 <aavyakumbhare@gmail.com>
2024-06-27 02:12:11 -0700

    Day 1 part 1 & 2 solution

---
+++ src/aoc_2023/day_01/mod.rs
@@ -0,0 +1,110 @@
+fn input() -> &'static str {
+    include_str!("input.txt")
+}
+
+fn collect_digits_in_line(line: &str) -> Vec<u32> {
+    line.chars().filter_map(|ch| ch.to_digit(10)).collect()
+}
+
+fn sum_calibrations(input: &str) -> u32 {
+    input
+        .trim()
+        .lines()
+        .map(collect_digits_in_line)
+        .map(|digits| digits.first().unwrap() * 10 + digits.last().unwrap())
+        .sum()
+}
+
+fn convert_words_to_digits(input: &str) -> String {
+    const NUMBERS: &[(&str, u32)] = &[
+        // ("eight", 8),
+        // ("seven", 7),
+        // ("three", 3),
+        // ("nine", 9),
+        // ("five", 5),
+        // ("four", 4),
+        // ("six", 6),
+        // ("two", 2),
+        // ("one", 1),
+
+        // ("one", 1),
+        // ("two", 2),
+        // ("six", 6),
+        // ("four", 4),
+        // ("five", 5),
+        // ("nine", 9),
+        // ("three", 3),
+        // ("seven", 7),
+        // ("eight", 8),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ];
+
+    let mut converted = String::new();
+
+    for line in input.lines() {
+        let bytes = line.as_bytes();
+
+        let mut i = 0;
+
+        'character_iter: while i < bytes.len() {
+            for &(word, digit) in NUMBERS {
+                if line
+                    .get(i..(i + word.len()))
+                    .is_some_and(|lookahead| lookahead == word)
+                {
+                    converted.push(char::from_digit(digit, 10).unwrap());
+                    i += word.len();
+
+                    continue 'character_iter;
+                }
+            }
+
+            converted.push(bytes[i] as char);
+
+            i += 1;
+        }
+
+        // let mut replaced_word_length = 0;
+        //
+        // for (index, ch) in line.trim().char_indices() {
+        //     'replace_word: for &(word, digit) in NUMBERS {
+        //         if line
+        //             .get(index..(index + word.len()))
+        //             .is_some_and(|lookahead| lookahead == word)
+        //         {
+        //             converted.push(char::from_digit(digit, 10).unwrap());
+        //             replaced_word_length = word.len();
+        //
+        //             break 'replace_word;
+        //         }
+        //     }
+        //
+        //     if replaced_word_length == 0 {
+        //         converted.push(ch);
+        //     } else {
+        //         replaced_word_length -= 1;
+        //     }
+        // }
+
+        converted.push('\n');
+    }
+
+    converted
+}
+
+pub fn part_one() -> u32 {
+    sum_calibrations(include_str!("input.txt"))
+}
+
+pub fn part_two() -> u32 {
+    let input = convert_words_to_digits(include_str!("test_input.txt"));
+    sum_calibrations(&input)
+}
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::fmt::Debug;
+
+mod aoc_2023;
+
+fn test_solution<T: Debug>(name: &str, mut func: impl FnMut() -> T) -> T {
+    let val = func();
+
+    let instant = std::time::Instant::now();
+
+    let iterations = 1;
+
+    for _ in 0..iterations {
+        let _ = func();
+    }
+
+    let elapsed = instant.elapsed();
+    let average_ms = elapsed.as_millis() as f64 / iterations as f64;
+
+    println!("Solution for {}: {:?} ({} ms)", name, val, average_ms);
+
+    val
+}
+
+fn main() {
+    test_solution("Day 1 Part 1", aoc_2023::day_01::part_one);
+    test_solution("Day 1 Part 2", aoc_2023::day_01::part_two);
+}
