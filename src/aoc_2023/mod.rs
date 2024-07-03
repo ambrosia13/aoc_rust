4eb0f17132a745a65d5e033dc6a427952f6a8ad0
ambrosia13 <aavyakumbhare@gmail.com>
2024-07-03 11:45:05 -0700

    Refactor

--- src/aoc_2023/mod.rs
+++ src/aoc_2023/mod.rs
@@ -1,1 +1,8 @@
+use crate::test_solution;
+
 pub mod day_01;
+
+pub fn bench() {
+    test_solution("Day 1 Part 1", day_01::part_one);
+    test_solution("Day 1 Part 2", day_01::part_two);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use std::fmt::Debug;
 
 mod aoc_2023;
 
-fn test_solution<T: Debug>(name: &str, mut func: impl FnMut() -> T) -> T {
+pub fn test_solution<T: Debug>(name: &str, mut func: impl FnMut() -> T) -> T {
     let val = func();
     let iterations = 500;
 
     let instant = std::time::Instant::now();
-    
+
     for _ in 0..iterations {
         std::hint::black_box(func());
     }
@@ -21,6 +21,5 @@ fn test_solution<T: Debug>(name: &str, mut func: impl FnMut() -> T) -> T {
 }
 
 fn main() {
-    test_solution("Day 1 Part 1", aoc_2023::day_01::part_one);
-    test_solution("Day 1 Part 2", aoc_2023::day_01::part_two);
+    aoc_2023::bench();
 }
